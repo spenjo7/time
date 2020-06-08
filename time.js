d33e573ed8357769a92ef480816b5e2a1c8fb6af
DESKTOP-S6PPVTO\User <jon.e.spencer@icloud.com>
2020-06-07 22:55:58 -0400

    Split functionality to allow for more options, also simplfied calculating timezone offset

--- time.js
+++ time.js
@@ -1,21 +1,49 @@
-const timestamp = ( d = new Date() ) =>{
-	let tz = d.getTimezoneOffset()/6; // that way we can get halves accurately
-	const tz_symb = tz > 0? '-' : '+';
-	tz = `0${tz}0`.slice(-4);
-	tz = `${tz_symb}${tz}`;
+/* 
+v1.0 2020-06-07  
+	- Creating to allow for easy custom timestamp outputs
+	- Spliting into multiple function so we can get partial outputs like just the date or just the time
+*/
 
-	const dx = [ // enforcing 2 chars for MM-DD HH:MM:SS
-		d.getMonth(),
-		d.getDate(),
-		d.getHours(),
-		d.getMinutes(),
-		d.getSeconds(),
-	].map( t => `0${t}`.slice(-2));
-	
-	const ms = `${d.getMilliseconds()}000`.slice(0,3); // enforcing 3 chars for  mmm
-	
-	//YYYY-MM-DD HH:MM:SS.MMM TZ
-	return 	`${d.getFullYear()}-${dx[0]}-${dx[1]} ${dx[2]}:${dx[3]}.${ms} ${tz}`;
+const leadingZero = ( val, length = 2 ) => { // Enforce leading zeros for numbers. Ex: 1 = 01
+	return `0${val}`.slice(length * -1);
 }
 
-module.exports = { timestamp };
+const getDateString = ( d = new Date() ) => { //YYYY-MM-DD only
+	const dx = [ // Enforce MM-DD
+		d.getMonth(),
+		d.getDate()
+	].map( t => leadingZero(t));
+	
+	return `${d.getFullYear()}-${dx[0]}-${dx[1]}`;
+}
+
+const getTimeString = ( d = new Date() ) => { // HH:MM:SS.mmm only
+	const dx = [ // Enforce HH:MM:SS
+		d.getHours(),
+		d.getMinutes(),
+		d.getSeconds()
+	].map( t => leadingZero(t));
+	const ms = `${d.getMilliseconds()}000`.slice(0,3); // Enforce 3 decimals for .mmm
+	
+	return 	`${dx[0]}:${dx[1]}:${dx[2]}.${ms}`;
+}
+
+const getTimezoneString = ( d = new Date() ) => { // ex: -0400
+	return d.toString().split('GMT')[1].split(' ')[0]; // Just using the pre-formated offset 
+}
+
+const dateTime = ( d = new Date() ) =>{ //YYYY-MM-DD HH:MM:SS.MMM // In case you don't want the TZ offset for some reason
+	return [
+		getDateString(d),
+		getTimeString(d)
+	].join(' ');
+}
+
+const timestamp = ( d = new Date() ) =>{ //YYYY-MM-DD HH:MM:SS.MMM TZ
+	return [
+		dateTime(d),
+		getTimezoneString(d)
+	].join(' ');
+}
+
+module.exports = { leadingZero, getDateString, getTimeString, getTimezoneString, dateTime, timestamp };
